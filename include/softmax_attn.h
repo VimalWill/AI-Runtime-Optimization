c459ae29db86e1ae5d36422ae5c7188600b76a69
VimalWill <vimalwilliam99@gmail.com>
2025-12-17 13:07:36 +0530

    updating the perf report

---
+++ include/softmax_attn.h
@@ -0,0 +1,16 @@
+// ================= softmax_atten.h =================
+#ifndef SOFTMAX_ATTN_H
+#define SOFTMAX_ATTN_H
+
+#include "runtime.h"
+
+enum SoftmaxAttnFuncType : bool {
+    SPAWN = 0,
+    SYNC = 1
+};
+
+struct SoftmaxAttnArgs {
+    
+}
+
+#endif
